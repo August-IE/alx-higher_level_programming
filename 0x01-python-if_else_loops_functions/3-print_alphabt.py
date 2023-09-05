3b81e2adac48010193382869a23897c253bb867d
August-IE <Aeromosele9@gmail.com>
2023-09-05 04:50:20 -0700

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -4,4 +4,4 @@
 not followed by a new line except letters 'q' and 'e'."""
 for letter in range(97, 123):
     if chr(letter) != 'q' and chr(letter) != 'e':
-        print(f"{chr(letter)}", end="")
+        print("{}".format(chr(letter)), end="")
