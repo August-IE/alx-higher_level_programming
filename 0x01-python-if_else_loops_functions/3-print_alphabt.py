9013dc8a5c137c171f03c3468c6fa239d5f7bdcd
August-IE <Aeromosele9@gmail.com>
2023-09-05 04:35:23 -0700

    Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""A program that prints the ASCII alphabet in lowercase,
+not followed by a new line except letters 'q' and 'e'."""
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print(f"{chr(letter)}", end="")
