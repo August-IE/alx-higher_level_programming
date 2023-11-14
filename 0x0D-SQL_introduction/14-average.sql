10bf20a8761d3cde6535c050d8a9beb642fe039f
August-IE <Aeromosele9@gmail.com>
2023-11-14 07:52:52 -0800

    Average

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+-- A script that computes the average of all records in the table second_table
+-- of the database hbtn_0c_0 in my MySQL server.
+-- The result column should be average
+SELECT AVG(`score`) AS `average`
+FROM `second_table`;
