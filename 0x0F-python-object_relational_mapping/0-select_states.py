ac32f70003a2dc048591c4ed009f15326a8dc159
August-IE <Aeromosele9@gmail.com>
2023-12-15 04:43:35 -0800

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
-# A script that lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+"""
+Script to retrieve and display all states from the hbtn_0e_0_usa database.
+"""
+
+# Required imports
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    # Establishing a connection to the database
     db = MySQLdb.connect(
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3]
-            )
+        user=sys.argv[1],    # MySQL username
+        passwd=sys.argv[2],  # MySQL password
+        db=sys.argv[3]       # Database name
+    )
+    
+    # Creating a cursor to execute SQL queries
     c = db.cursor()
+    
+    # Executing a SELECT query to retrieve columns from the 'states' table
     c.execute("SELECT `id`, `name` FROM `states` ORDER BY `states`.`id` ASC")
+    
+    # Fetching all rows from the executed query
     states = c.fetchall()
+    
+    # Displaying each retrieved state
     for state in states:
         print(state)
