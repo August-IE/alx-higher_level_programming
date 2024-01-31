4ae414ba9e6563945e73ee653d2a0b7374c90a21
August-IE <Aeromosele9@gmail.com>
2024-01-31 02:58:57 -0800

    Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,31 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
 request(url, function (error, response, body) {
-  if (!error) {
-    let characters = JSON.parse(body).characters;
+  if (!error && response.statusCode === 200) {
+    const filmData = JSON.parse(body);
+    const characters = filmData.characters;
     printCharacters(characters, 0);
+  } else {
+    console.error('Error fetching movie data:', error);
   }
 });
 
 function printCharacters (characters, index) {
+  if (index >= characters.length) {
+    return; // Stop recursion when characters list is exhausted
+  }
+
   request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
+    if (!error && response.statusCode === 200) {
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+      printCharacters(characters, index + 1); // Move to the next character
+    } else {
+      console.error('Error fetching character data:', error);
     }
   });
 }
